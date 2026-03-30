703eac887aace416c58d3999cc77dd340c686606
danhmujar <danhmichael.mujar@gmail.com>
2026-03-30 18:57:35 +0800

    test: update performance tests to use new default preview URL

--- tests/performance.spec.js
+++ tests/performance.spec.js
@@ -2,7 +2,7 @@ import { test, expect } from '@playwright/test';
 
 test.describe('Performance: Eye Tracking Hardware Acceleration', () => {
     test.beforeEach(async ({ page }) => {
-        await page.goto('http://localhost:5173');
+        await page.goto('http://localhost:4173/Calculator/');
         await page.waitForSelector('#pupil1');
     });
 
